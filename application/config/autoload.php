6557a45fd72cf54795fbe1cc9180949d78526f2b
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-24 01:26:12 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database','session','ion_auth', 'form_validation');
+$autoload['libraries'] = array('database','session','form_validation');
 
 /*
 | -------------------------------------------------------------------
