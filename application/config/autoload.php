b1b06c4f629f2cb513a0f48f3a6cfc2f8acef517
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-24 00:50:55 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -87,7 +87,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'tglIndo_helper');
+$autoload['helper'] = array('url');
 
 
 /*
