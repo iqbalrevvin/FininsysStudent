0904e3d6a40b45fed94aa55a554addd96977465d
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-25 02:45:07 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Authentication';
+$route['default_controller'] = 'home';
 //PESERTA DIDIK
 
-
+$route['Home'] = '';
 $route['404_override'] = 'error';
 $route['translate_uri_dashes'] = TRUE;
